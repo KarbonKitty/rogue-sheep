7c9e010d446f339e779ed0ba2572502cf394cc39
KarbonKitty <308lapua.magnum@gmail.com>
2020-02-10 13:13:18 +0100

    Add minimal PCG implementation as Helper

---
+++ Helpers/PCG.cs
@@ -0,0 +1,39 @@
+namespace RogueSheep.Helpers
+{
+    /*
+    * PCG Random Number Generation for C#
+    * 
+    * Original algorithm for C/C++ (c) by Melissa O'Neill <oneill@pcg-random.org>
+    * and PCG Project contributors.
+    * 
+    * Original licensed under Apache 2.0 or MIT license.
+    * 
+    * For additional information about the PCG random number generation scheme,
+    * original source code and binary builds, as well as original license details
+    * visit http://www.pcg-random.org/.
+    * 
+    */
+    public class PCGRandomMinimal
+    {
+        private ulong state;
+        private ulong stream;
+
+        public PCGRandomMinimal(ulong initState, ulong initStream)
+        {
+            state = 0;
+            stream = initStream << 1 | 1;
+            Next();
+            state += initState;
+            Next();
+        }
+
+        public uint Next()
+        {
+            ulong oldState = state;
+            state = oldState * 6364136223846793005 + stream;
+            uint xorshifted = (uint)(((oldState >> 18) ^ oldState) >> 27);
+            uint rot = (uint)(oldState >> 59);
+            return (xorshifted >> (int)rot) | (xorshifted << ((int)(-rot) & 31));
+        }
+    }
+}
