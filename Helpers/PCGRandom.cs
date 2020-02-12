4a2627a84b2de44ef5a90d39a357b7ee8680fea7
KarbonKitty <308lapua.magnum@gmail.com>
2020-02-12 12:55:02 +0100

    Fix bug in RNG helper

--- Helpers/PCGRandom.cs
+++ Helpers/PCGRandom.cs
@@ -39,7 +39,7 @@ namespace RogueSheep.Helpers
                 uint next = Next();
                 if (next > threshold)
                 {
-                    return next % threshold;
+                    return next % maxValue;
                 }
             }
         }
