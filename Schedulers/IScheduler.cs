75b21aa9f75ff4a4f2b3a1dafa02b8184897263a
KarbonKitty <308lapua.magnum@gmail.com>
2020-02-14 12:23:10 +0100

    Implement scheduler interface and basic scheduler

---
+++ Schedulers/IScheduler.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace RogueSheep.Schedulers
+{
+    public interface IScheduler<T>
+    {
+        T Next();
+        void Add(T item);
+        void Add(params T[] items);
+        void AddRange(IEnumerable<T> items);
+        bool Remove(T item);
+        void Clear();
+    }
+}
---
+++ Schedulers/RoundRobinScheduler.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RogueSheep.Schedulers
+{
+    public class RoundRobinScheduler<T> : IScheduler<T>
+    {
+        private List<T> list;
+        private int lastIndex = 0;
+
+        public RoundRobinScheduler()
+        {
+            list = new List<T>(32);
+        }
+
+        public T Next()
+        {
+            if (!list.Any())
+            {
+                throw new InvalidOperationException("Next can only be called on non-empty scheduler");
+            }
+
+            lastIndex++;
+            if (lastIndex >= list.Count)
+            {
+                lastIndex = 0;
+            }
+            return list[lastIndex];
+        }
+
+        public void Add(T item) => list.Add(item);
+
+        public void Add(params T[] items) => list.AddRange(items);
+
+        public void AddRange(IEnumerable<T> items) => list.AddRange(items);
+
+        public bool Remove(T item)
+        {
+            var index = list.IndexOf(item);
+            if (index < lastIndex)
+            {
+                lastIndex--;
+            }
+            return list.Remove(item);
+        }
+
+        public void Clear() => list.Clear();
+    }
+}
